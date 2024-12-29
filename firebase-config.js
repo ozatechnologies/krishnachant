09d81fdd1734fca38eb3e257e7263559b05e845c
Dev <100591237+ozatechnologies@users.noreply.github.com>
2024-12-30 01:10:49 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Secure Firebase Configuration Loader
+(function() {
+    // Create a secure configuration object
+    window.getFirebaseConfig = function() {
+        return {
+            apiKey: "AIzaSyC3mwFH49WiG4Swjmo-1JV9wjAMBIDkLA4",
+            authDomain: "pushtiwords.firebaseapp.com",
+            projectId: "pushtiwords",
+            databaseURL: "https://pushtiwords-default-rtdb.firebaseio.com",
+            storageBucket: "pushtiwords.appspot.com",
+            messagingSenderId: "460696747514",
+            appId: "1:460696747514:web:40e94e966bdfcfe624b843"
+        };
+    };
+})();
